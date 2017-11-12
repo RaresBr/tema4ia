24fed4b3bf35c8bfeb5a78081103ac72bdb2d6f5
RaresBr <rares.bradea@gmail.com>
2017-11-12 16:04:07 +0200

    added part2. forward check

--- bk.py
+++ bk.py
@@ -1,20 +1,21 @@
 from graphlib import *
+import copy
 
 def bk(asg):
-    if len(get_unnassigned_nodes(asg)) == 0:
+    if len(get_unassigned_nodes(asg)) == 0:
         return asg
     
-    var = get_mrv_nodes(get_unnassigned_nodes(asg))[0]
+    var = get_mrv_nodes(get_unassigned_nodes(asg))[0]
     print('--------------------state info--------------------')
     print("node to assign: ", var)
     print("assigment: ",asg)
     print("----------mrv list----------")
-    for i in range(0,len(get_mrv_nodes(get_unnassigned_nodes(asg)))):
-        print('node',get_mrv_nodes(get_unnassigned_nodes(asg))[i])
-        print('value',get_mrv(get_mrv_nodes(get_unnassigned_nodes(asg))[i]))
+    for i in range(0,len(get_mrv_nodes(get_unassigned_nodes(asg)))):
+        print('node',get_mrv_nodes(get_unassigned_nodes(asg))[i])
+        print('value',get_mrv(get_mrv_nodes(get_unassigned_nodes(asg))[i]))
     print("--------------------------------------------------")
-    print("unassg:", get_unnassigned_nodes(asg))
-    print("mrv sorted unassg: ",get_mrv_nodes(get_unnassigned_nodes(asg)))
+    print("unassg:", get_unassigned_nodes(asg))
+    print("mrv sorted unassg: ",get_mrv_nodes(get_unassigned_nodes(asg)))
     for color in colors:
         print("pot sa atribui culoarea:?",color)
         index = get_index_of_node(var)
@@ -26,10 +27,10 @@ def bk(asg):
 def get_mrv(node):
     sum = 0
     for color in colors:
-        if(is_safe(node,color)):
+        if is_safe(node,color):
             sum = sum +1
     return sum
-def get_unnassigned_nodes(nodesdict):
+def get_unassigned_nodes(nodesdict):
     lista = [item for item in nodesdict.keys() if nodesdict.get(item) == None]
     return lista
 def get_mrv_nodes(nodes):
@@ -40,10 +41,69 @@ def get_index_of_node(node):
     for i in range(0,len(nodes)) :
         if node == nodes[i]:
             return i
-bk(colors_of_nodes)
+def get_domain(node):
+    domain = []
+    for color in colors:
+        if is_safe(node,color):
+            domain.append(color)
+    return domain
+def get_all_domains():
+    domains = []
+    for node in G.nodes():
+        domains.append(get_domain(node))
+    return domains
+def get_unassigned_neighbors(node,nodesdict):
+    unasg = get_unassigned_nodes(nodesdict)
+    unasg_neighbors = []
+    all_neighbors = G.neighbors(node)
+    for neighbor in all_neighbors:
+        if neighbor in unasg:
+            unasg_neighbors.append(neighbor)
+    return unasg_neighbors
+def forward_check(asg,domains):
+    if len(get_unassigned_nodes(asg))==0:
+        return asg
+    node = get_unassigned_nodes(asg)[0]
+    index = get_index_of_node(node)
+    for color in domains[index]:
+        if is_safe(node,color):
+            colors_of_nodes[list(G.nodes)[index]]=color
+            domains_copy = copy.deepcopy(domains)
+            for unassigned_neighbor in get_unassigned_neighbors(node,asg):
+                for color in domains_copy[get_index_of_node(unassigned_neighbor)]:
+                    if not is_safe(unassigned_neighbor,color):
+                        domains_copy[get_index_of_node(unassigned_neighbor)].remove(color)
+            all_neighbors_not_empty = True
+            for unassigned_neighbor in get_unassigned_neighbors(node,asg):
+                if len(domains_copy[get_index_of_node(unassigned_neighbor)]) == 0:
+                    all_neighbors_not_empty = False
+                    break
+            if all_neighbors_not_empty == True:
+                result = forward_check(asg,domains_copy)
+                if result is not None:
+                    return result
+            colors_of_nodes[list(G.nodes)[index]] = None
+    return None
+
+
+# print(get_all_domains())
+# colors_of_nodes[1]='Red'
+# dc = copy.deepcopy(get_all_domains())
+# for node in G.nodes():
+    # dc[get_index_of_node(node)].remove('Green')
+    # if len(dc[get_index_of_node(node)]) != 0:
+        # print('not empty')
+    # else:
+        # print('empty')
+    # print(node,get_index_of_node(node),dc[get_index_of_node(node)])
+    
+# forward_check(colors_of_nodes,get_all_domains())
+
+# bk(colors_of_nodes)
 print(colors_of_nodes)
 draw_stuff()
-for node in G.nodes():
-    print(node)
-print(colors_of_nodes)
+
+# for node in G.nodes():
+    # print(node)
+# print(colors_of_nodes)
 
